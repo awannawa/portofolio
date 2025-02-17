fdf7593d8e19135a71c25ec13c1e489edaccd784
awannawa <kurniawanofficiall@gmail.com>
2025-02-17 11:39:39 +0700

    unload empty string di backend database google sheet

--- js/port.js
+++ js/port.js
@@ -8,51 +8,55 @@ fetch(
     const container = document.getElementById("gallery");
 
     data.forEach((item) => {
-      const anchor = document.createElement("a");
-      //   anchor.setAttribute("data-lg-size", "1280-720");
-      anchor.classList.add(
-        "gallery-item",
-        "filtr-item",
-        "col-lg-3",
-        "col-md-6",
-        "col-sm-12"
-      );
-      anchor.setAttribute("data-category", item.media_type);
-      if (item.media_type === "video") {
-        anchor.setAttribute("data-iframe", "true");
-        anchor.setAttribute(
-          "data-src",
-          "https://drive.google.com/file/d/" + item.id_media + "/preview"
+      if (item.nama !== "") {
+        const anchor = document.createElement("a");
+        //   anchor.setAttribute("data-lg-size", "1280-720");
+        anchor.classList.add(
+          "gallery-item",
+          "filtr-item",
+          "col-lg-3",
+          "col-md-6",
+          "col-sm-12"
         );
-      } else {
-        anchor.setAttribute(
-          "data-src",
-          "https://drive.google.com/thumbnail?id=" + item.id_media + "&sz=s1080"
+        anchor.setAttribute("data-category", item.media_type);
+        if (item.media_type === "video") {
+          anchor.setAttribute("data-iframe", "true");
+          anchor.setAttribute(
+            "data-src",
+            "https://drive.google.com/file/d/" + item.id_media + "/preview"
+          );
+        } else {
+          anchor.setAttribute(
+            "data-src",
+            "https://drive.google.com/thumbnail?id=" +
+              item.id_media +
+              "&sz=s1080"
+          );
+        }
+
+        anchor.setAttribute("data-sub-html", item.keterangan);
+
+        const img = document.createElement("img");
+        img.setAttribute("alt", item.nama);
+        img.classList.add("img-responsive", "border-box-shadow");
+        img.setAttribute(
+          "src",
+          "https://lh3.googleusercontent.com/d/" + item.id_thumbnail + "=s200"
         );
+
+        const span = document.createElement("span");
+        span.classList.add("item-desc");
+        span.textContent = item.media_type;
+
+        const span2 = document.createElement("span");
+        span2.classList.add("item-desc2");
+        span2.textContent = item.nama;
+
+        anchor.appendChild(img);
+        anchor.appendChild(span);
+        anchor.appendChild(span2);
+        container.appendChild(anchor);
       }
-
-      anchor.setAttribute("data-sub-html", item.keterangan);
-
-      const img = document.createElement("img");
-      img.setAttribute("alt", item.nama);
-      img.classList.add("img-responsive", "border-box-shadow");
-      img.setAttribute(
-        "src",
-        "https://lh3.googleusercontent.com/d/" + item.id_thumbnail + "=s200"
-      );
-
-      const span = document.createElement("span");
-      span.classList.add("item-desc");
-      span.textContent = item.media_type;
-
-      const span2 = document.createElement("span");
-      span2.classList.add("item-desc2");
-      span2.textContent = item.nama;
-
-      anchor.appendChild(img);
-      anchor.appendChild(span);
-      anchor.appendChild(span2);
-      container.appendChild(anchor);
     });
 
     // Inisialisasi lightGallery setelah data selesai dimuat
